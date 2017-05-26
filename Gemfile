9d520bd8993151e003dbc9ddde47e2bf48b07923
Paul M Furley <paul@paulfurley.com>
2017-05-26 15:20:16 +0100

    Add middleman deploy

--- Gemfile
+++ Gemfile
@@ -6,3 +6,5 @@ gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
 
 gem 'middleman-livereload'
+
+gem 'middleman-deploy', '~> 2.0.0.alpha'
--- config.rb
+++ config.rb
@@ -45,4 +45,13 @@ page '/*.txt', layout: false
 #   activate :minify_javascript
 # end
 
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  deploy.branch   = 'build'
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
 activate :livereload
